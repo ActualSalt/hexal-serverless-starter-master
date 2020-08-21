754b5fd89866f4c2bcafdd01c725d0ec26c67b56
MinLee <minlee0702@gmail.com>
2020-08-21 18:00:35 -0500

    Update hexalPut.js

--- hexal-lambda/hexalPut.js
+++ hexal-lambda/hexalPut.js
@@ -7,11 +7,13 @@ exports.handler = async (event, context) => {
     let responseBody = "";
     let statusCode = 0;
 
+    const { id, productname } = JSON.parse(event.body);
+
     const params = {
         TableName: "Products",
         Item: {
-            id: '12345',
-            productname: 'Solar Panels'
+            id: id,
+            productname: productname
         }
     };
 
