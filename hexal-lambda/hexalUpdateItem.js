f64f710022dff90f2e035a02fd1d39ce857dbfa1
MinLee <minlee0702@gmail.com>
2020-08-22 23:16:46 -0500

    Update hexalUpdateItem.js

--- hexal-lambda/hexalUpdateItem.js
+++ hexal-lambda/hexalUpdateItem.js
@@ -2,41 +2,41 @@
 const AWS = require('aws-sdk');
 
 exports.handler = async (event, context) => {
-    const documentClient = new AWS.DynamoDB.DocumentClient();
+  const documentClient = new AWS.DynamoDB.DocumentClient();
 
-    let responseBody = "";
-    let statusCode = 0;
+  let responseBody = "";
+  let statusCode = 0;
 
-    const { id, productname } = JSON.parse(event.body);
+  const { id, productname } = JSON.parse(event.body);
 
-    const params = {
-      TableName: "Products",
-      Key: {
-        id: id
-      },
-      UpdateExpression: "set productname = :n",
-      ExpressionAttributeValues: {
-        ":n": productname
-      },
-      ReturnValues: "UPDATED_NEW"
-    };
-    try {
-        const data = await documentClient.update(params).promise();
-        responseBody = JSON.stringify(data);
-        statusCode = 204;
-    }catch (err){
-        responseBody = `Unable to update product: ${err}`;
-        statusCode = 403;
-    }
+  const params = {
+    TableName: "Products",
+    Key: {
+      id: id
+    },
+    UpdateExpression: "set productname = :n",
+    ExpressionAttributeValues: {
+      ":n": productname
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+  try {
+    const data = await documentClient.update(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 204;
+  } catch (err) {
+    responseBody = `Unable to update product: ${err}`;
+    statusCode = 403;
+  }
 
-    const response = {
-        statusCode: statusCode,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: responseBody
-    };
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: responseBody
+  };
 
-    return response;
+  return response;
 };
 
